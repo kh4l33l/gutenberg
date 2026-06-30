b6fad1166834e28c1f66259227a71d919e26f4fe
Andrew Serong <14988353+andrewserong@users.noreply.github.com>
2026-06-30 16:55:56 +1000

    Update labels

--- packages/block-library/src/gallery/edit.js
+++ packages/block-library/src/gallery/edit.js
@@ -704,7 +704,7 @@ export default function GalleryEdit( props ) {
 							variant="secondary"
 							onClick={ dynamic.enableDynamicMode }
 						>
-							{ __( 'Use images attached to the post' ) }
+							{ __( 'Attached images' ) }
 						</Button>
 					) }
 				</MediaPlaceholder>
